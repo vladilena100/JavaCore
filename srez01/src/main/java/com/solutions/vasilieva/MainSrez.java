433ac4492916cf92916914646b66c820b1c3db76
Vladilena Vasilieva <vladylena.vasylieva@nure.ua>
2021-05-07 19:52:51 +0300

    Srez 01

---
+++ srez01/src/main/java/com/solutions/vasilieva/MainSrez.java
@@ -0,0 +1,12 @@
+package com.solutions.vasilieva;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MainSrez {
+    MainSrez c = new MainSrez();
+    private static final Logger LOG = LogManager.getLogger(MainSrez.class);
+    public static void main(String[] args) {
+        //LOG.info(" " + c.srez());
+    }
+}
