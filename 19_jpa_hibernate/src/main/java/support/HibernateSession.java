e6af8ac7def0cac41304accf0ef7ff222b9c021e
vasylieva <vladylena.vasylieva@nixsolutions.com>
2022-01-03 17:21:16 +0200

    task 3 hibernate

--- 19_jpa_hibernate/src/main/java/support/HibernateSession.java
+++ 19_jpa_hibernate/src/main/java/support/HibernateSession.java
@@ -1,4 +1,35 @@
 package support;
 
+import model.Role;
+import model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateSession {
+
+    private static HibernateSession hibernateSession;
+    private SessionFactory sessionFactory;
+
+    private HibernateSession () {
+        initDBConfig();
+    }
+
+    public static synchronized HibernateSession getInstance() {
+        if (hibernateSession == null) {
+            hibernateSession = new HibernateSession();
+        }
+        return hibernateSession;
+    }
+
+    public Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    private void initDBConfig() {
+        Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(User.class);
+        configuration.addAnnotatedClass(Role.class);
+        sessionFactory = configuration.buildSessionFactory();
+    }
 }
